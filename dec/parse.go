485934d7a360a6ab86184d0cd261f61ab7e8a9be
Andrei Petkun <avpetkun@gmail.com>
2024-09-12 04:12:57 +0400

    decimal parse funcs check overflow

--- dec/parse.go
+++ dec/parse.go
@@ -1,6 +1,9 @@
 package dec
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 var errEmptyNumber = fmt.Errorf("parsing empty number")
 
@@ -9,6 +12,7 @@ func ParseUint64(s []byte) (v uint64, err error) {
 		return 0, errEmptyNumber
 	}
 	var c byte
+	var n uint64
 	for _, c = range s {
 		c -= '0'
 		if c > 9 {
@@ -16,7 +20,12 @@ func ParseUint64(s []byte) (v uint64, err error) {
 			err = fmt.Errorf("invalid number symbol '%s' in number %s", string(c), s)
 			return
 		}
-		v = v*10 + uint64(c)
+		n = v*10 + uint64(c)
+		if n < v {
+			err = fmt.Errorf("too big uint64 number '%s'", s)
+			return
+		}
+		v = n
 	}
 	return
 }
@@ -31,6 +40,7 @@ func ParseInt64(s []byte) (v int64, err error) {
 		neg = true
 	}
 	var c byte
+	var n int64
 	for _, c = range s {
 		c -= '0'
 		if c > 9 {
@@ -38,7 +48,12 @@ func ParseInt64(s []byte) (v int64, err error) {
 			err = fmt.Errorf("invalid number symbol '%s' in number %s", string(c), s)
 			return
 		}
-		v = v*10 + int64(c)
+		n = v*10 + int64(c)
+		if n < v && n != math.MinInt64 {
+			err = fmt.Errorf("too big int64 number '%s'", s)
+			return
+		}
+		v = n
 	}
 	if neg {
 		v = -v
