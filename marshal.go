9d34f2de4dd3ccf453bdc03af196ae1181db4377
Andrei Petkun <avpetkun@gmail.com>
2024-09-13 18:28:18 +0400

    marshal: quote strings

--- marshal.go
+++ marshal.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"unicode/utf8"
 	"unsafe"
 
 	"github.com/avpetkun/jessy-go/dec"
@@ -185,7 +186,7 @@ func pointerEncoder(deep, offset int, t reflect.Type, isEmbedded, isOmitempty bo
 		v = unsafe.Add(v, offset)
 		vp := *(*uintptr)(v)
 		if vp == 0 {
-			return append(dst, "null"...), nil
+			return append(dst, 'n', 'u', 'l', 'l'), nil
 		}
 		return elemEncoder(dst, unsafe.Pointer(vp))
 	}
@@ -193,7 +194,7 @@ func pointerEncoder(deep, offset int, t reflect.Type, isEmbedded, isOmitempty bo
 
 func mapEncoder(deep, offset int, t reflect.Type, isOmitempty bool) encoder {
 	return func(dst []byte, v unsafe.Pointer) ([]byte, error) {
-		return append(dst, "null"...), nil
+		return append(dst, 'n', 'u', 'l', 'l'), nil
 	}
 }
 
@@ -204,12 +205,10 @@ func stringEncoder(offset int, isOmitempty bool) encoder {
 			if isOmitempty {
 				return dst, nil
 			}
-			return append(dst, `""`...), nil
+			return append(dst, '"', '"'), nil
 		}
 		data := unsafe.Slice(h.Data, h.Len)
-		dst = append(dst, '"')
-		dst = append(dst, data...)
-		dst = append(dst, '"')
+		dst = appendString(dst, data)
 		return dst, nil
 	}
 }
@@ -224,7 +223,7 @@ func sliceEncoder(deep, offset int, t reflect.Type, isOmitempty bool) encoder {
 			if isOmitempty {
 				return dst, nil
 			}
-			return append(dst, `[]`...), nil
+			return append(dst, '[', ']'), nil
 		}
 		dst = append(dst, '[')
 		for i := range h.Len {
@@ -444,16 +443,16 @@ func boolEncoder(offset int, isOmitempty bool) encoder {
 	if isOmitempty {
 		return func(dst []byte, v unsafe.Pointer) ([]byte, error) {
 			if *(*bool)(unsafe.Add(v, offset)) {
-				return append(dst, "true"...), nil
+				return append(dst, 't', 'r', 'u', 'e'), nil
 			}
 			return dst, nil
 		}
 	}
 	return func(dst []byte, v unsafe.Pointer) ([]byte, error) {
 		if *(*bool)(unsafe.Add(v, offset)) {
-			return append(dst, "true"...), nil
+			return append(dst, 't', 'r', 'u', 'e'), nil
 		}
-		return append(dst, "false"...), nil
+		return append(dst, 'f', 'a', 'l', 's', 'e'), nil
 	}
 }
 
@@ -479,4 +478,178 @@ func marshalerEncoder(offset int, t reflect.Type) encoder {
 		}
 		return append(dst, data...), nil
 	}
+}
+
+// from encoding/json.appendString
+func appendString(dst, src []byte) []byte {
+	const hex = "0123456789abcdef"
+
+	dst = append(dst, '"')
+	start := 0
+	srcLen := len(src)
+	for i := 0; i < srcLen; i++ {
+		if b := src[i]; b < utf8.RuneSelf {
+			if safeSet[b] {
+				continue
+			}
+			dst = append(dst, src[start:i]...)
+			switch b {
+			case '\\', '"':
+				dst = append(dst, '\\', b)
+			case '\b':
+				dst = append(dst, '\\', 'b')
+			case '\f':
+				dst = append(dst, '\\', 'f')
+			case '\n':
+				dst = append(dst, '\\', 'n')
+			case '\r':
+				dst = append(dst, '\\', 'r')
+			case '\t':
+				dst = append(dst, '\\', 't')
+			default:
+				// This encodes bytes < 0x20 except for \b, \f, \n, \r and \t.
+				// If escapeHTML is set, it also escapes <, >, and &
+				// because they can lead to security holes when
+				// user-controlled strings are rendered into JSON
+				// and served to some browsers.
+				dst = append(dst, '\\', 'u', '0', '0', hex[b>>4], hex[b&0xF])
+			}
+			i++
+			start = i
+			continue
+		}
+		// TODO(https://go.dev/issue/56948): Use generic utf8 functionality.
+		// For now, cast only a small portion of byte slices to a string
+		// so that it can be stack allocated. This slows down []byte slightly
+		// due to the extra copy, but keeps string performance roughly the same.
+		n := len(src) - i
+		if n > utf8.UTFMax {
+			n = utf8.UTFMax
+		}
+		c, size := utf8.DecodeRuneInString(string(src[i : i+n]))
+		if c == utf8.RuneError && size == 1 {
+			dst = append(dst, src[start:i]...)
+			dst = append(dst, `\ufffd`...)
+			i += size
+			start = i
+			continue
+		}
+		// U+2028 is LINE SEPARATOR.
+		// U+2029 is PARAGRAPH SEPARATOR.
+		// They are both technically valid characters in JSON strings,
+		// but don't work in JSONP, which has to be evaluated as JavaScript,
+		// and can lead to security holes there. It is valid JSON to
+		// escape them, so we do so unconditionally.
+		// See https://en.wikipedia.org/wiki/JSON#Safety.
+		if c == '\u2028' || c == '\u2029' {
+			dst = append(dst, src[start:i]...)
+			dst = append(dst, '\\', 'u', '2', '0', '2', hex[c&0xF])
+			i += size
+			start = i
+			continue
+		}
+		i += size
+	}
+	dst = append(dst, src[start:]...)
+	dst = append(dst, '"')
+	return dst
+}
+
+var safeSet = [utf8.RuneSelf]bool{
+	' ':      true,
+	'!':      true,
+	'"':      false,
+	'#':      true,
+	'$':      true,
+	'%':      true,
+	'&':      true,
+	'\'':     true,
+	'(':      true,
+	')':      true,
+	'*':      true,
+	'+':      true,
+	',':      true,
+	'-':      true,
+	'.':      true,
+	'/':      true,
+	'0':      true,
+	'1':      true,
+	'2':      true,
+	'3':      true,
+	'4':      true,
+	'5':      true,
+	'6':      true,
+	'7':      true,
+	'8':      true,
+	'9':      true,
+	':':      true,
+	';':      true,
+	'<':      true,
+	'=':      true,
+	'>':      true,
+	'?':      true,
+	'@':      true,
+	'A':      true,
+	'B':      true,
+	'C':      true,
+	'D':      true,
+	'E':      true,
+	'F':      true,
+	'G':      true,
+	'H':      true,
+	'I':      true,
+	'J':      true,
+	'K':      true,
+	'L':      true,
+	'M':      true,
+	'N':      true,
+	'O':      true,
+	'P':      true,
+	'Q':      true,
+	'R':      true,
+	'S':      true,
+	'T':      true,
+	'U':      true,
+	'V':      true,
+	'W':      true,
+	'X':      true,
+	'Y':      true,
+	'Z':      true,
+	'[':      true,
+	'\\':     false,
+	']':      true,
+	'^':      true,
+	'_':      true,
+	'`':      true,
+	'a':      true,
+	'b':      true,
+	'c':      true,
+	'd':      true,
+	'e':      true,
+	'f':      true,
+	'g':      true,
+	'h':      true,
+	'i':      true,
+	'j':      true,
+	'k':      true,
+	'l':      true,
+	'm':      true,
+	'n':      true,
+	'o':      true,
+	'p':      true,
+	'q':      true,
+	'r':      true,
+	's':      true,
+	't':      true,
+	'u':      true,
+	'v':      true,
+	'w':      true,
+	'x':      true,
+	'y':      true,
+	'z':      true,
+	'{':      true,
+	'|':      true,
+	'}':      true,
+	'~':      true,
+	'\u007f': true,
 }
