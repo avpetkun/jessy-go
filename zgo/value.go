693daa2729e4f8913dadd76dd20f1a639ef8ebbe
Andrei Petkun <avpetkun@gmail.com>
2024-09-24 17:55:12 +0400

    fix interface Marshaller as value

--- zgo/value.go
+++ zgo/value.go
@@ -54,7 +54,8 @@ func NewAnyInterfacerFromRType(rType reflect.Type) func(valPtr unsafe.Pointer) a
 }
 
 func NewInterfacerFromRType[I any](rType reflect.Type) func(valPtr unsafe.Pointer) I {
-	i, ok := NewRValueFromRType(rType, nil).Interface().(I)
+	var v I
+	i, ok := NewRValueFromRType(rType, unsafe.Pointer(&v)).Interface().(I)
 	if !ok {
 		return nil
 	}
